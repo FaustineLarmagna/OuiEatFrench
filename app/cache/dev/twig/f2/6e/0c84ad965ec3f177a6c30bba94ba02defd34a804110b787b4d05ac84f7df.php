69d97da353ccba29e53591a40e9f30bcbddff998
Flambard <flambard@flambard-HP-EliteBook-8560p.(none)>
2014-06-18 11:50:07 +0200

    Create symfony project

---
+++ app/cache/dev/twig/f2/6e/0c84ad965ec3f177a6c30bba94ba02defd34a804110b787b4d05ac84f7df.php
@@ -0,0 +1,116 @@
+<?php
+
+/* @WebProfiler/Profiler/toolbar_js.html.twig */
+class __TwigTemplate_f26e0c84ad965ec3f177a6c30bba94ba02defd34a804110b787b4d05ac84f7df extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<div id=\"sfwdt";
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "\" class=\"sf-toolbar\" style=\"display: none\"></div>
+";
+        // line 2
+        $this->env->loadTemplate("@WebProfiler/Profiler/base_js.html.twig")->display($context);
+        // line 3
+        echo "<script>/*<![CDATA[*/
+    (function () {
+        ";
+        // line 5
+        if (("top" == (isset($context["position"]) ? $context["position"] : $this->getContext($context, "position")))) {
+            // line 6
+            echo "            var sfwdt = document.getElementById('sfwdt";
+            echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+            echo "');
+            document.body.insertBefore(
+                document.body.removeChild(sfwdt),
+                document.body.firstChild
+            );
+        ";
+        }
+        // line 12
+        echo "
+        Sfjs.load(
+            'sfwdt";
+        // line 14
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "',
+            '";
+        // line 15
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_wdt", array("token" => (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")))), "html", null, true);
+        echo "',
+            function(xhr, el) {
+                el.style.display = -1 !== xhr.responseText.indexOf('sf-toolbarreset') ? 'block' : 'none';
+
+                if (el.style.display == 'none') {
+                    return;
+                }
+
+                if (Sfjs.getPreference('toolbar/displayState') == 'none') {
+                    document.getElementById('sfToolbarMainContent-";
+        // line 24
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'none';
+                    document.getElementById('sfToolbarClearer-";
+        // line 25
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'none';
+                    document.getElementById('sfMiniToolbar-";
+        // line 26
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'block';
+                } else {
+                    document.getElementById('sfToolbarMainContent-";
+        // line 28
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'block';
+                    document.getElementById('sfToolbarClearer-";
+        // line 29
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'block';
+                    document.getElementById('sfMiniToolbar-";
+        // line 30
+        echo twig_escape_filter($this->env, (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")), "html", null, true);
+        echo "').style.display = 'none';
+                }
+            },
+            function(xhr) {
+                if (xhr.status !== 0) {
+                    confirm('An error occurred while loading the web debug toolbar (' + xhr.status + ': ' + xhr.statusText + ').\\n\\nDo you want to open the profiler?') && (window.location = '";
+        // line 35
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("_profiler", array("token" => (isset($context["token"]) ? $context["token"] : $this->getContext($context, "token")))), "html", null, true);
+        echo "');
+                }
+            },
+            {'maxTries': 5}
+        );
+    })();
+/*]]>*/</script>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "@WebProfiler/Profiler/toolbar_js.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  91 => 35,  83 => 30,  79 => 29,  75 => 28,  70 => 26,  66 => 25,  62 => 24,  50 => 15,  42 => 12,  26 => 3,  24 => 2,  19 => 1,  98 => 40,  93 => 9,  80 => 41,  46 => 14,  44 => 9,  40 => 8,  32 => 6,  27 => 4,  22 => 1,  127 => 28,  120 => 20,  117 => 19,  110 => 22,  108 => 19,  105 => 18,  102 => 17,  94 => 34,  90 => 32,  88 => 6,  84 => 29,  82 => 28,  78 => 40,  76 => 17,  73 => 16,  64 => 13,  61 => 12,  56 => 11,  53 => 10,  47 => 8,  36 => 7,  58 => 17,  52 => 14,  49 => 13,  43 => 11,  41 => 5,  33 => 3,  30 => 5,  25 => 35,);
+    }
+}
