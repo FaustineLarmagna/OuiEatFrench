bd333c24f29882487fe0a9b11da379e1054ac315
Flambard <flambard@flambard-HP-EliteBook-8560p.(none)>
2014-09-11 13:59:00 +0200

    Landing page working

--- web/ouieatfrench/theme/assets2/js/script.js
+++ web/ouieatfrench/theme/assets2/js/script.js
@@ -1,101 +1,73 @@
 $(document).ready(function (){
     $("#howitworks").click(function (){
         //$(this).animate(function(){
-            $('html, body').animate({
-                scrollTop: $("#howit").offset().top
-            }, 1000);
+        $('html, body').animate({
+            scrollTop: $("#howit").offset().top
+        }, 1000);
         //});
     });
 
     $("#howitworks2").click(function (){
         //$(this).animate(function(){
-            $('html, body').animate({
-                scrollTop: $("#howit2").offset().top
-            }, 1000);
+        $('html, body').animate({
+            scrollTop: $("#howit2").offset().top
+        }, 1000);
         //});
     });
 
     $("#join-us").click(function (){
         //$(this).animate(function(){
-            $('html, body').animate({
-                scrollTop: $("#forms").offset().top
-            }, 1000);
+        $('html, body').animate({
+            scrollTop: $("#forms").offset().top
+        }, 1000);
         //});
     });
 
     $("#qui-sommes-nous").click(function (){
         //$(this).animate(function(){
-            $('html, body').animate({
-                scrollTop: $("#team").offset().top
-            }, 1000);
+        $('html, body').animate({
+            scrollTop: $("#team").offset().top
+        }, 1000);
         //});
     });
 
     (function($){
-	    var jump=function(e)
-	    {
-	       if (e){
-	           e.preventDefault();
-	           var target = $(this).attr("href");
-	       }else{
-	           var target = location.hash;
-	       }
+        var jump=function(e)
+        {
+            if (e){
+                e.preventDefault();
+                var target = $(this).attr("href");
+            }else{
+                var target = location.hash;
+            }
 
-	       $('html,body').animate(
-	       {
-	           scrollTop: $(target).offset().top
-	       },1000,function()
-	       {
-	           location.hash = target;
-	       });
+            $('html,body').animate(
+                {
+                    scrollTop: $(target).offset().top
+                },1000,function()
+                {
+                    location.hash = target;
+                });
 
-	    }
+        }
 
-	    $('html, body').hide()
+        $('html, body').hide()
 
-	    $(document).ready(function()
-	    {
-	        $('a[href^=#]').bind("click", jump);
+        $(document).ready(function()
+        {
+            $('a[href^=#]').bind("click", jump);
 
-	        if (location.hash){
-	            setTimeout(function(){
-	                $('html, body').scrollTop(0).show()
-	                jump()
-	            }, 0);
-	        }else{
-	          $('html, body').show()
-	        }
-	    });
-	  
-	})(jQuery)
+            if (location.hash){
+                setTimeout(function(){
+                    $('html, body').scrollTop(0).show()
+                    jump()
+                }, 0);
+            }else{
+                $('html, body').show()
+            }
+        });
 
-	$(function () 
-	{
-	    var div = $('#headerwrap #slider');
-	    var backgrounds = [
-	      'url(assets2/img/murephoto2.jpg)', 
-	      'url(assets2/img/coccinelle.jpg)',
-	      'url(assets2/img/abricot.jpg)',
-	      'url(assets2/img/fleur.jpg)'];
-	    var current = 0;
+    })(jQuery)
 
-	    function nextBackground() {
-	    	div.fadeOut(100, function(){
-	    		div.css(
-	            'background',
-	        backgrounds[current = ++current % backgrounds.length]);
-	    		div.css('background-size','cover');
-				div.css('background-position', 'center center');
-	    		div.fadeIn(1000);
-	    	});
-	        
 
-	        setTimeout(nextBackground, 7000);
-	    }
-	    setTimeout(nextBackground, 7000);
-	    div.css('background', backgrounds[0]);
-	    div.css('background-size','cover');
-		div.css('background-position', 'center center');
-	});
-    
 });
