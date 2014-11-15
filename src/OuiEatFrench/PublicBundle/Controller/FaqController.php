adbdaed0a863c15ee19b782f13e9aea805f05eed
Flambard <flambard@flambard-HP-EliteBook-8560p.(none)>
2014-11-15 14:58:56 +0100

    Add faq and product in public bundle

--- src/OuiEatFrench/PublicBundle/Controller/FaqController.php
+++ src/OuiEatFrench/PublicBundle/Controller/FaqController.php
@@ -8,8 +8,8 @@ class FaqController extends Controller
 {
     public function indexAction()
     {
-        $faq = $this->getDoctrine()->getRepository('OuiEatFrenchAdminBundle:Faq')->findBy(array('disable' => true));
-        $data["faq"] = $faq;
+        $faqs = $this->getDoctrine()->getRepository('OuiEatFrenchAdminBundle:Faq')->findBy(array('disable' => true));
+        $data["faqs"] = $faqs;
         return $this->render('OuiEatFrenchPublicBundle:Faq:index.html.twig', $data);
     }
 }
---
+++ src/OuiEatFrench/PublicBundle/Controller/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OuiEatFrench\PublicBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ProductController extends Controller
+{
+    public function indexAction()
+    {
+        $products = $this->getDoctrine()->getRepository('OuiEatFrenchAdminBundle:Product')->findBy(array('parentProduct' => null));
+        $data["products"] = $products;
+        return $this->render('OuiEatFrenchPublicBundle:Product:index.html.twig', $data);
+    }
+}
