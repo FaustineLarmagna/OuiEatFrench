007760646573e94cc32d67cf1faa1e6657ede214
Bastien Henry <bastien.henry@supinternet.fr>
2015-03-27 17:35:14 +0100

    add datafixtures

---
+++ src/OuiEatFrench/AdminBundle/DataFixtures/ORM/LoadCategoryData.php
@@ -0,0 +1,36 @@
+<?php 
+
+namespace OuiEatFrench\AdminBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use OuiEatFrench\AdminBundle\Entity\Category;
+
+class LoadCategoryData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        
+        foreach($this->feedCategories() as $categoryName)
+        {
+        	$categoryAdmin = new Category();
+	        $categoryAdmin->setName($categoryName);
+
+	        $manager->persist($categoryAdmin);
+        
+        }
+        $manager->flush();
+    }
+
+    public function feedCategories()
+    {
+    	return array(
+    		"fruits",
+    		"legumes",
+    		"autres",
+    		);
+    }
+}
---
+++ src/OuiEatFrench/AdminBundle/DataFixtures/ORM/LoadSeasonData.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace OuiEatFrench\AdminBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use OuiEatFrench\AdminBundle\Entity\Season;
+
+class LoadSeasonData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        
+        foreach($this->feedSeason() as $seasonName)
+        {
+        	$seasonAdmin = new Season();
+	        $seasonAdmin->setName($seasonName);
+
+	        $manager->persist($seasonAdmin);
+        
+        }
+        $manager->flush();
+    }
+
+    public function feedSeason()
+    {
+    	return array(
+    		"ete",
+    		"printemps",
+    		"hiver",
+            "automne"
+    		);
+    }
+}
